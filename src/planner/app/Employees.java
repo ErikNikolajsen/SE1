ac4016b6951d4df35a10dd3ea8cc69c1aaf224e4
Erik Nikolajsen <erik.nikolajsen@gmail.com>
2020-05-04 21:24:15 +0200

    Deleted asda user from database

--- src/planner/app/Employees.java
+++ src/planner/app/Employees.java
@@ -1,7 +1,13 @@
 package planner.app;
 
 import java.util.*;
-
+	/*
+	 * TODO
+	 * Add make leader menu item
+	 * deleteEmployee()
+	 * 	- make error if the user deleted is a leader
+	 * 	- delete all the related activities of the deleted user
+	 */
 public class Employees {
 	
 	public static void displayEmployees() {
