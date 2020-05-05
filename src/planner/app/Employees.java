6c4e309c1a735615a53a446e6dd623017bdbe473
Erik Nikolajsen <erik.nikolajsen@gmail.com>
2020-05-05 09:27:35 +0200

    Fixed merge conflict

--- src/planner/app/Employees.java
+++ src/planner/app/Employees.java
@@ -1,13 +1,20 @@
 package planner.app;
 
 import java.util.*;
-
+	/*
+	 * TODO
+	 * Add make leader menu item
+	 * deleteEmployee()
+	 * 	- make error if the user deleted is a leader
+	 * 	- delete all the related activities of the deleted user
+	 */
 public class Employees {
 	
 	public static void displayEmployees() {
 		System.out.println("Choose menu item:");
 		System.out.println("1. Add employee");
-		System.out.println("2. Exit");
+		System.out.println("2. Delete employee");
+		System.out.println("3. Exit");
 		
 		
 		
@@ -19,27 +26,38 @@ public class Employees {
 				addEmployeeView();
 			} else if (n.equals("2")) {
 				validInput = true;
+				deleteEmployeeView();
+			} else if (n.equals("3")) {
+				validInput = true;
 				Menu.displayMenu();
 			} else {
 				System.out.println("Error: invalid input");
 			}
 		}
+	
 	}
 	
 	private static void addEmployeeView() {
 		String name;
 		String initials;
 		
-		boolean validInput = false;
-		while (validInput == false) {
-			System.out.println("Choose name");
-			name = Model.scan.nextLine();
-			System.out.println("Choose initials");
-			initials = Model.scan.nextLine();
-			addEmployee(name, initials);
-			validInput = true;
-		}
+		System.out.println("Choose name");
+		name = Model.scan.nextLine();
+		System.out.println("Choose initials");
+		initials = Model.scan.nextLine();
+		addEmployee(name, initials);
+		displayEmployees();
 	}
+	
+	private static void deleteEmployeeView() {
+		String initials;
+		
+		System.out.println("Choose the initials of the employee you wish to delete");
+		initials = Model.scan.nextLine();
+		deleteEmployee(initials);
+		displayEmployees();
+	}
+	
 	
 	//Controller events
 	private static void addEmployee(String name, String initials) {
@@ -65,7 +83,20 @@ public class Employees {
                       "VALUES ('" + initials.toUpperCase() + "', '" + name + "');"; 
 	
 			SQLiteJDBC.createStatement(sql);
-			System.out.println("Success: the employee " + name + " was added to the database");
+			System.out.println("Success: the employee " + initials.toUpperCase() + " was added to the database");
+		}
+	}
+	
+	private static void deleteEmployee(String initials) {
+		if (!SQLiteJDBC.selectEmployeesInitials().contains(initials.toUpperCase())) {
+			System.out.println("Error: no employee with those initials exists in the database");
+		} else if (initials.toUpperCase().equals(Model.currentUser)) {
+			System.out.println("Error: it is not possible to delete yourself");
+		} else {
+			String sql = "DELETE FROM employees WHERE initials = '" + initials.toUpperCase() + "';";
+			
+			SQLiteJDBC.createStatement(sql);
+			System.out.println("Success: the employee " + initials.toUpperCase() + " was deleted from the database");
 		}
 	}
 }
